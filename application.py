8f4c7dc0f12ab6b9d57572868e88d359de8273af
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-10-16 05:50:09 +0530

    flask: static file adde , served index.html from template folder

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            static_folder='./static',
+            template_folder='./templates')
 
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
